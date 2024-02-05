5a0d61fb9f3efca994951bd4831e54a6a5975121
Hrithika <rhrithikha04@gmail.com>
2024-02-05 22:49:06 +0530

    started

---
+++ foodio/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+
+
+const AppLayout=()=>{
+  return (
+    <div className="app">
+     { /*header*/}
+
+    </div>
+  )
+
+  
+}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
